f7d5a14f466a5aa27b8f9b97a66351c3e0e40fe4
Federico "Morg" Pareschi <morgawr@gmail.com>
2021-08-27 10:44:11 +0900

    test kunyomi map

---
+++ kun/kunmap.js
@@ -0,0 +1,31 @@
+var outb = document.getElementById('output');
+
+const options = {
+  threshold: 0,
+  keys: ["kun"]
+}
+
+async function fetchData(){
+   let response = await fetch('kun_map.json');
+   let data = await response.json();
+   return data;
+}
+
+let json_data = await fetchData();
+
+const fuse = new Fuse(json_data, options);
+
+document.getElementById('textbox').addEventListener('input', function(){
+  let v = this.value;
+  outb.innerHTML = "";
+
+  let results = fuse.search(v);
+
+  let output = "";
+  console.log(results);
+  results.forEach(function(result, index) {
+    output += "<li><b>" + result.item["kun"] + "</b> → " + result.item["kanji"] + "</li>";
+  })
+  outb.innerHTML = output;
+});
+
