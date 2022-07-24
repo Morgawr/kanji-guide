1e4a0c42c4ca26c83f58b1f9f169ecf230807181
Federico 'Morg' Pareschi <morgawr@gmail.com>
2022-07-24 11:10:29 +0900

    Add support for frequency in jukugo nodes

--- code/graph/jukugo_node.py
+++ code/graph/jukugo_node.py
@@ -1,6 +1,7 @@
 """Module with an implementation of a jukugo word node."""
 from graph.node import Node
 from kanji.word import Word
+from corpus.corpus import Corpus
 
 
 class JukugoNode(Node):
@@ -9,6 +10,7 @@ class JukugoNode(Node):
     def unique_id(self) -> str:
         return ','.join(self.value.get_all_representations())
 
-    def __init__(self, value: Word):
+    def __init__(self, value: Word, corpus: Corpus):
         super().__init__()
         self.value = value
+        self.frequency = corpus.get_freq(str(self.value.default_representation))
--- code/kanji/word.py
+++ code/kanji/word.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 
 @dataclass
@@ -14,7 +14,15 @@ class _KanjiFragment:
 class Word:
     """Represents a Japanese word with possible alternative kanji spellings."""
 
-    def __init__(self, word: str):
+    def __init__(self, word: str, default_representation: Optional[str] = None):
+        """Init method.
+
+        Args:
+            word: the kana representation of the word.
+            default_representation: the common/canonical representation of the
+                word when it's not in kana form. For implementation reasons,
+                this is not guaranteed to match the kanji segments of the word.
+        """
         self.word: str = word
         # Alternative/irregular kana spelling (like ありがとう vs ありがと)
         self.alternative: List[str] = list()
@@ -22,6 +30,12 @@ class Word:
         # has a number of kanji and a starting and ending point for how many
         # characters are going to be hidden by the kanji parts.
         self.segments: List[List[_KanjiFragment]] = list()
+        # The default representation is useful when we need to search for a
+        # certain word in some corpus and need to match its kanji form to avoid
+        # homonyms.
+        self.default_representation = None
+        if default_representation:
+            self.default_representation = default_representation
 
     def add_segment(self, *fragments: Tuple(str, int, int)) -> None:
         """Adds a new segment to the word segment list.
