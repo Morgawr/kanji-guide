bfcdd00ba5a6ab14861e95c119bcf7bf58eab337
Federico 'Morg' Pareschi <morgawr@gmail.com>
2022-07-24 10:17:10 +0900

    Add initial corpus tooling

---
+++ code/corpus/README.md
@@ -0,0 +1,4 @@
+# Corpus
+
+This module contains various data related to frequency of kanji/word as analyzed
+from the corpus data provided in csv format.
--- code/graph/JukugoNode.py
+++ code/graph/JukugoNode.py
@@ -2,13 +2,11 @@ from graph.Node import Node
 from kanji.Word import Word
 
 
-class KanaNode(Node):
+class JukugoNode(Node):
 
     def UniqueID(self) -> str:
         return ','.join(self.value.GetAllRepresentations())
 
-    # TODO(morg): Handle kana nodes with multiple kana words like transitive vs
-    # intransitive pairs being considered a single word group.
     def __init__(self, value: Word):
         super().__init__()
         self.value = value
